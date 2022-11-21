270f953650adf9df7c43e3f113729623dcf4953e
Gabriele <g.nocchi@hotmail.com>
2022-11-21 10:56:00 -0700

    Add files via upload

--- batch_structure_analysis.sh
+++ batch_structure_analysis.sh
@@ -1,9 +1,15 @@
 for FILE in *.vcf.gz
 do
 
+# MAF 0.05 filter to analyse structure
+
+/data/programs/vcftools_0.1.13/bin/vcftools --gzvcf $FILE --max-missing 0.7 --maf 0.05 --minQ 30 --minGQ 20 --minDP 5 --max-alleles 2 --recode --recode-INFO-all
+
+
+
 # VCF to plink format for faststructure
 
-/data/programs/plink2 --vcf $FILE --allow-extra-chr --threads 4 --make-bed --out $FILE\_plink_data
+/data/programs/plink2 --vcf out.recode.vcf --allow-extra-chr --make-bed --out $FILE\_plink_data
 
 # faststructure K from 1 to 10
 
@@ -21,11 +27,13 @@ python /lu213/gabriele.nocchi/fastStructure-master/structure.py -K 10 --input=$F
 
 # quick and dirty PCA
 
-/data/programs/plink2 --vcf $FILE --pca 10 var-wts --allow-extra-chr --out $FILE\_pca
+/data/programs/plink2 --vcf out.recode.vcf --pca 10 var-wts --allow-extra-chr --out $FILE\_pca
 
 # Heterozygosity assessment
 
-/data/programs/vcftools_0.1.13/bin/vcftools --gzvcf $FILE --het --out $FILE\_heterozygosity
+/data/programs/vcftools_0.1.13/bin/vcftools --vcf out.recode.vcf --het --out $FILE\_heterozygosity
 
+
+rm out.recode.vcf
 
 done
