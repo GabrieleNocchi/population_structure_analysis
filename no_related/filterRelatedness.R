b160f2c72482bfe9a429e21c86d4744a86ee7b5c
Gabriele <g.nocchi@hotmail.com>
2022-11-29 13:37:55 -0700

    Add files via upload

--- no_related/filterRelatedness.R
+++ no_related/filterRelatedness.R
@@ -1,6 +1,6 @@
 library(plinkQC)
 
 a <- read.table("plink.genome", h = T)
-indiv <- relatednessFilter(a,relatednessTh = 0.125)
+indiv <- relatednessFilter(a,relatednessTh = 0.2)
 
 write.table(indiv$failIDs, file = "to_remove.txt", row.names = FALSE, quote = FALSE, col.names = F, sep = "\t")
