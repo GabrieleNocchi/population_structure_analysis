83732763b7bdfb7a424581ef940935e6891bb172
Gabriele <g.nocchi@hotmail.com>
2023-09-10 15:28:38 -0600

    Update batch_structure_analysis_no_relatedness_alternative.sh

--- no_related/batch_structure_analysis_no_relatedness_alternative.sh
+++ no_related/batch_structure_analysis_no_relatedness_alternative.sh
@@ -2,7 +2,6 @@ for FILE in *.vcf.gz
 do
 
 # MAF 0.05 filter to analyse structure
-
 /data/programs/vcftools_0.1.13/bin/vcftools --gzvcf $FILE --max-missing 0.7 --maf 0.05 --minQ 30 --minGQ 20 --minDP 5 --max-alleles 2 --recode --recode-INFO-all
 
 # Need to set SNPs IDs
@@ -14,33 +13,17 @@ do
 # make a VCF only with unlinked SNPs (LD r2 max 0.4)
 /data/programs/plink --vcf ID.vcf --allow-extra-chr --extract pruned.prune.in --double-id --recode vcf --out pruned
 
-
-
 # VCF to plink format (bed, bim, fam) and remove related
-
 /data/programs/plink --vcf pruned.vcf --allow-extra-chr --make-bed --double-id --rel-cutoff 0.2 --out $FILE\_plink_data
 
-
-
 # faststructure K from 1 to 10
-
-python2.7 /lu213/gabriele.nocchi/fastStructure-master/structure.py -K 1 --input=$FILE\_plink_data --output=$FILE\_fast_output --full --seed=100 
-python2.7 /lu213/gabriele.nocchi/fastStructure-master/structure.py -K 2 --input=$FILE\_plink_data --output=$FILE\_fast_output --full --seed=100 
-python2.7 /lu213/gabriele.nocchi/fastStructure-master/structure.py -K 3 --input=$FILE\_plink_data --output=$FILE\_fast_output --full --seed=100 
-python2.7 /lu213/gabriele.nocchi/fastStructure-master/structure.py -K 4 --input=$FILE\_plink_data --output=$FILE\_fast_output --full --seed=100 
-python2.7 /lu213/gabriele.nocchi/fastStructure-master/structure.py -K 5 --input=$FILE\_plink_data --output=$FILE\_fast_output --full --seed=100 
-python2.7 /lu213/gabriele.nocchi/fastStructure-master/structure.py -K 6 --input=$FILE\_plink_data --output=$FILE\_fast_output --full --seed=100 
-python2.7 /lu213/gabriele.nocchi/fastStructure-master/structure.py -K 7 --input=$FILE\_plink_data --output=$FILE\_fast_output --full --seed=100 
-python2.7 /lu213/gabriele.nocchi/fastStructure-master/structure.py -K 8 --input=$FILE\_plink_data --output=$FILE\_fast_output --full --seed=100 
-python2.7 /lu213/gabriele.nocchi/fastStructure-master/structure.py -K 9 --input=$FILE\_plink_data --output=$FILE\_fast_output --full --seed=100 
-python2.7 /lu213/gabriele.nocchi/fastStructure-master/structure.py -K 10 --input=$FILE\_plink_data --output=$FILE\_fast_output --full --seed=100
-
+for K in {1..10}
+do
+python2.7 /lu213/gabriele.nocchi/fastStructure-master/structure.py -K $K --input=$FILE\_plink_data --output=$FILE\_fast_output --full --seed=100 
+done
 
 # quick and dirty PCA
-
 /data/programs/plink2 --bfile $FILE\_plink_data --pca 10 var-wts --allow-extra-chr --out $FILE\_pca
-
-
 
 rm *.vcf
 rm out.log
