6d43861fc8063615e36c05645ced7d1ee5d0e986
Gabriele <g.nocchi@hotmail.com>
2023-09-10 15:39:39 -0600

    Update filterRelatedness.R

--- no_related/filterRelatedness.R
+++ no_related/filterRelatedness.R
@@ -1,6 +1,5 @@
 library(plinkQC)
 
-a <- read.table("plink.genome", h = T)
-indiv <- relatednessFilter(a,relatednessTh = 0.2)
-
+my_data <- read.table("plink.genome", h = T)
+indiv <- relatednessFilter(my_data,relatednessTh = 0.2)
 write.table(indiv$failIDs, file = "to_remove.txt", row.names = FALSE, quote = FALSE, col.names = F, sep = "\t")
